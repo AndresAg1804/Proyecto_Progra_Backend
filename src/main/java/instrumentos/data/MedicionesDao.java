284af233d07c64422d111a22696e113865597451
AndresAg1804 <anner.angulo.gutierrez@est.una.ac.cr>
2023-11-03 10:54:58 -0600

    Backend probablemente listo

---
+++ src/main/java/instrumentos/data/MedicionesDao.java
@@ -0,0 +1,114 @@
+package instrumentos.data;
+
+
+import instrumentos.logic.Mediciones;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MedicionesDao {
+    Database db;
+
+    public MedicionesDao() {
+        db = Database.instance();
+    }
+
+    public int create(Mediciones e) throws Exception {
+        String sql = "insert into " +
+                "Mediciones " +
+                "(referencia, lectura, calibracion_id) " +
+                "values(?,?,?)";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, e.getReferencia());
+        stm.setString(2, String.valueOf(e.getLectura()));
+        stm.setString(3, String.valueOf(e.getCalibraciones().getNumero()));
+
+        ResultSet keys = db.executeUpdateWithKeys(stm);
+        keys.next();
+        return keys.getInt(1);
+    }
+
+    public Mediciones read(String serie) throws Exception {
+        String sql = "select * from " +
+                "Mediciones i " +
+                "where i.serie=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, serie);
+        ResultSet rs = db.executeQuery(stm);
+        if (rs.next()) {
+            return from(rs, "i");
+        } else {
+            throw new Exception("Medicion NO EXISTE");
+        }
+    }
+
+    public void update(Mediciones e) throws Exception {
+        String sql = "update " +
+                "Mediciones " +
+                "set lectura=?" +
+                "where medida=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, String.valueOf(e.getLectura()));
+        stm.setString(2, e.getMedida());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Medicion NO EXISTE");
+        }
+
+    }
+
+    public void delete(Mediciones e) throws Exception {
+        String sql = "delete " +
+                "from Mediciones " +
+                "where medida=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, e.getMedida());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Medicion NO EXISTE");
+        }
+    }
+
+    public List<Mediciones> search(Mediciones e) throws Exception {
+        List<Mediciones> resultado = new ArrayList<Mediciones>();
+        String sql = "select * from " +
+                "Mediciones i " +
+                "where i.medida like ?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, "%" + e.getMedida() + "%");
+        ResultSet rs = db.executeQuery(stm);
+        while (rs.next()) {
+            resultado.add(from(rs, "i"));
+        }
+        return resultado;
+    }
+
+    public List<Mediciones> searchMedicionesByCalibracion(String id){
+        List<Mediciones> med = new ArrayList<>();
+        String sql = "select * from " +
+                "Mediciones i " +
+                "where i.calibracion_id=?";
+        try {
+            PreparedStatement stm = db.prepareStatement(sql);
+            stm.setString(1, id);
+            ResultSet rs = db.executeQuery(stm);
+            while (rs.next()) {
+                med.add(from(rs, "i"));
+            }
+        } catch (Exception e) {
+        }
+        return med;
+    }
+
+    public Mediciones from(ResultSet rs, String alias) throws Exception {
+        CalibracionesDao ed = new CalibracionesDao();
+        Mediciones e = new Mediciones();
+        e.setMedida(rs.getString(alias + ".medida"));
+        //e.setCalibraciones(ed.from(rs,"t"));
+        e.setReferencia(rs.getString(alias + ".referencia"));
+        e.setLectura(Integer.parseInt(rs.getString(alias+".lectura")));
+        return e;
+    }
+}
---
+++ src/main/java/instrumentos/logic/Service.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instrumentos.logic;
+
+import instrumentos.data.CalibracionesDao;
+import instrumentos.data.InstrumentoDao;
+import instrumentos.data.MedicionesDao;
+import instrumentos.data.TipoInstrumentoDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service implements IService{
+    private static Service theInstance;
+    public static Service instance(){
+        if (theInstance==null){
+            theInstance=new Service();
+        }
+        return theInstance;
+    }
+    private TipoInstrumentoDao tipoInstrumentoDao;
+    private InstrumentoDao instrumentoDao;
+    private CalibracionesDao calibracionesDao;
+    private MedicionesDao medicionesDao;
+
+    public Service() {
+        try{
+            tipoInstrumentoDao = new TipoInstrumentoDao();
+            instrumentoDao = new InstrumentoDao();
+            calibracionesDao = new CalibracionesDao();
+            medicionesDao = new MedicionesDao();
+            //calibra = new ArrayList<>();
+        }
+        catch(Exception e){
+        }
+    }
+
+    //================= TIPOS DE INSTRUMENTO ============
+    public void create(TipoInstrumento e)throws Exception{
+        tipoInstrumentoDao.create(e);
+    }
+
+    public TipoInstrumento read(TipoInstrumento e) throws Exception{
+        return tipoInstrumentoDao.read(e.getCodigo());
+    }
+
+    public void update(TipoInstrumento e)throws Exception{
+        tipoInstrumentoDao.update(e);
+    }
+
+    public void delete(TipoInstrumento e)throws Exception{
+        tipoInstrumentoDao.delete(e);
+    }
+
+    public List<TipoInstrumento> search(TipoInstrumento e) throws Exception {
+        return tipoInstrumentoDao.search(e);
+    }
+
+    // ------------ INSTRUMENTOS -------------
+
+    public void create(Instrumento e) throws Exception{
+        instrumentoDao.create(e);
+    }
+
+    public Instrumento read(Instrumento e) throws Exception{
+        return instrumentoDao.read(e.getSerie());
+    }
+
+    public void update(Instrumento e)throws Exception{
+        instrumentoDao.update(e);
+    }
+
+    public void delete(Instrumento e)throws Exception{
+        instrumentoDao.delete(e);
+    }
+
+    public List<Instrumento> search(Instrumento v) throws Exception {
+        return instrumentoDao.search(v);
+    }
+
+    //================= Calibraciones ============
+
+    public void create(Calibraciones e) throws Exception{
+        int id = calibracionesDao.create(e);
+        e.setNumero(String.valueOf(id));
+        e.getMedicionesList().forEach((med) -> {   // for each medicion in calibracion
+            try {
+               int medida =  medicionesDao.create(med);
+               med.setMedida(String.valueOf(medida));
+            } catch (Exception ex) {
+            }
+        });
+    }
+
+    public Calibraciones read(Calibraciones e) throws Exception{
+        return calibracionesDao.read(e.getNumero());
+    }
+
+    public void update(Calibraciones e)throws Exception{
+        calibracionesDao.update(e);
+    }
+
+    public void delete(Calibraciones e)throws Exception{
+        calibracionesDao.delete(e);
+    }
+
+    public List<Calibraciones> search(Calibraciones v) throws Exception {
+        return calibracionesDao.search(v);
+    }
+
+    //================= Mediciones ============
+
+    public void create(Mediciones e) throws Exception{
+        medicionesDao.create(e);
+    }
+
+    public Mediciones read(Mediciones e) throws Exception{
+        return medicionesDao.read(e.getMedida());
+    }
+
+    public void update(Mediciones e)throws Exception{
+        medicionesDao.update(e);
+    }
+
+    public void delete(Mediciones e)throws Exception{
+        medicionesDao.delete(e);
+    }
+
+    public List<Mediciones> search(Mediciones v) throws Exception {
+        return medicionesDao.search(v);
+    }
+
+    public List<Mediciones> searchMedicionesByCalibracion(String id){
+        List<Mediciones> med = new ArrayList<>();
+        med = medicionesDao.searchMedicionesByCalibracion(id);
+        return med;
+    }
+
+    public List<Calibraciones> searchCalibracionesByInstrumento(String serie){
+        List<Calibraciones> cali = new ArrayList<>();
+        cali = calibracionesDao.searchCalibracionesByInstrumento(serie);
+        for(Calibraciones c : cali){
+            c.setMedicionesList(searchMedicionesByCalibracion(c.getNumero()));
+        }
+        return cali;
+    }
+}
